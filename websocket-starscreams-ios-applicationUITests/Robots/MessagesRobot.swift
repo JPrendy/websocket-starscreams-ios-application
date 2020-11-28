815bc39d6f6d2ceed52cbc5aab6ede0f1aa352bc
James Prendergast <jamprendergast@gmail.com>
2020-11-28 18:44:50 +0000

    updated the README to include extensive documentation for setup and testing

--- websocket-starscreams-ios-applicationUITests/Robots/MessagesRobot.swift
+++ websocket-starscreams-ios-applicationUITests/Robots/MessagesRobot.swift
@@ -13,13 +13,11 @@ public final class MessagesRobot {
     @discardableResult
     public func tapWebsocketButton() -> Self {
         app.buttons["Send Data"].tap()
-        sleep(3)
         return self
     }
     
     public func tapWebsocketButton2() -> Self {
         app.buttons["Send Data 2"].tap()
-        sleep(3)
         return self
     }
     
@@ -29,6 +27,7 @@ public final class MessagesRobot {
     
     @discardableResult
     public func assertSentWebsocketText(text: String) -> Self {
+        sleep(5)
         let actualApiText = app.staticTexts.matching(identifier: "webSocketTextLabel").firstMatch.text
         let identifier = "webSocketTextLabel"
         // Will need to change this depending on where the identifier is set `app.staticTexts`, aka to check a button we would have `app.buttons`
