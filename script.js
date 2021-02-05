53f1b80d7dc07a9f42daab7f076f9b5e760cedf5
James Prendergast <jamprendergast@gmail.com>
2021-02-05 17:47:10 +0000

    test

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+module.exports = ({github, context}) => {
+    return context.payload.client_payload.value
+  }
+  
   // Get the existing comments.
   const {data: comments} = await github.issues.listComments({
     owner: context.repo.owner,
