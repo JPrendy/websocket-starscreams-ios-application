45cb15b3b9042bc473df513c0e43a988d1f914af
James Prendergast <jamprendergast@gmail.com>
2021-02-05 21:53:32 +0000

    test

---
+++ simple.js
@@ -0,0 +1,31 @@
+module.exports = ( ...args ) => {
+	console.log( args );
+};
+
+//    // Get the existing comments.
+//    const {data: comments} = await github.issues.listComments({
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     issue_number: context.payload.number,
+//   })
+//   // Find any comment already made by the bot.
+//   const botComment = comments.find(comment => comment.user.id)
+//   console.log(botComment)
+  
+//   const commentBody = "test"
+//   if (botComment) {
+//     await github.issues.updateComment({
+//       owner: context.repo.owner,
+//       repo: context.repo.repo,
+//       comment_id: botComment.id,
+//       body: commentBody
+//     })
+//   } else {
+//     await github.issues.createComment({
+//       owner: context.repo.owner,
+//       repo: context.repo.repo,
+//       issue_number: context.payload.number,
+//       body: commentBody
+//     })
+//   }
+
